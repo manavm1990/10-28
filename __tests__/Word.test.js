c688dafa99aecb13225849b2fcf123e425dbfac0
Manav Misra <manavm@visionwebsoft.com>
2022-12-13 17:18:13 -0600

    test: :recycle: naming

--- __tests__/Word.test.js
+++ __tests__/Word.test.js
@@ -15,26 +15,26 @@ describe("Word class", () => {
 
   describe("guessLetter", () => {
     it("Correct guess returns true", () => {
-      expect(new Word("fish").guessLetter("i")).toBe(true);
+      expect(new Word("fish").isLetterFound("i")).toBe(true);
     });
 
     it("Incorrect guess returns false", () => {
-      expect(new Word("fish").guessLetter("o")).toBe(false);
+      expect(new Word("fish").isLetterFound("o")).toBe(false);
     });
   });
 
   describe("guessedCorrectly ", () => {
     it("returns true if all letters are correct", () => {
       const word = new Word("hi");
-      word.guessLetter("h");
-      word.guessLetter("i");
-      expect(word.guessedCorrectly()).toBe(true);
+      word.isLetterFound("h");
+      word.isLetterFound("i");
+      expect(word.isWordGuessed()).toBe(true);
     });
     it("returns false if at least one letter is incorrect", () => {
       const word = new Word("hi");
-      word.guessLetter("h");
-      word.guessLetter("a");
-      expect(word.guessedCorrectly()).toBe(false);
+      word.isLetterFound("h");
+      word.isLetterFound("a");
+      expect(word.isWordGuessed()).toBe(false);
     });
   });
 });
