66836345ce8aecfb10c61b266cdb693666a416b7
Manav Misra <manavm@visionwebsoft.com>
2022-12-13 17:26:04 -0600

    refactor: :recycle: Game

--- app/models/Game.js
+++ app/models/Game.js
@@ -18,7 +18,7 @@ export default class Game {
   nextWord() {
     const randWord = words[Math.floor(Math.random() * words.length)];
     this.currentWord = new Word(randWord);
-    console.log("\n" + this.currentWord.toString() + "\n");
+    console.info("\n" + this.currentWord.toString() + "\n");
     this.makeGuess();
   }
 
@@ -27,7 +27,7 @@ export default class Game {
     this.askForLetter().then(() => {
       // If the user has no guesses remaining after this guess, show them the word, ask if they want to play again
       if (this.guessesLeft < 1) {
-        console.log(
+        console.info(
           'No guesses left! Word was: "' +
             this.currentWord.getSolution() +
             '"\n'
@@ -35,8 +35,8 @@ export default class Game {
         this.askToPlayAgain();
 
         // If the user guessed all letters of the current word correctly, reset guessesLeft to 10 and get the next word
-      } else if (this.currentWord.guessedCorrectly()) {
-        console.log("You got it right! Next word!");
+      } else if (this.currentWord.isWordGuessed()) {
+        console.info("You got it right! Next word!");
         this.guessesLeft = 10;
         this.nextWord();
 
@@ -48,57 +48,52 @@ export default class Game {
   }
 
   // Asks the user if they want to play again after running out of guessesLeft
-  askToPlayAgain() {
-    inquirer
-      .prompt([
-        {
-          type: "confirm",
-          name: "choice",
-          message: "Play Again?",
-        },
-      ])
-      .then((val) => {
-        // If the user says yes to another game, play again, otherwise quit the game
-        if (val.choice) {
-          this.play();
-        } else {
-          this.quit();
-        }
-      });
+  async askToPlayAgain() {
+    const { choice } = await inquirer.prompt([
+      {
+        type: "confirm",
+        name: "choice",
+        message: "Play Again?",
+      },
+    ]);
+
+    if (choice) {
+      this.play();
+    } else {
+      this.quit();
+    }
   }
 
   // Prompts the user for a letter
-  askForLetter() {
-    return inquirer
-      .prompt([
-        {
-          type: "input",
-          name: "choice",
-          message: "Guess a letter!",
-          // The users guess must be a number or letter
-          validate: (val) => /[a-z1-9]/gi.test(val),
-        },
-      ])
-      .then((val) => {
-        // If the user's guess is in the current word, log that they chose correctly
-        const didGuessCorrectly = this.currentWord.guessLetter(val.choice);
-        if (didGuessCorrectly) {
-          console.log(chalk.green("\nCORRECT!!!\n"));
+  async askForLetter() {
+    const letter = await inquirer.prompt([
+      {
+        type: "input",
+        name: "choice",
+        message: "Guess a letter!",
+        // The users guess must be a number or letter
+        validate: (val) => /[a-z1-9]/gi.test(val),
+      },
+    ]);
 
-          // Otherwise decrement `guessesLeft`, and let the user know how many guesses they have left
-        } else {
-          this.guessesLeft--;
-          console.log(chalk.red("\nINCORRECT!!!\n"));
-          console.log(this.guessesLeft + " guesses remaining!!!\n");
-        }
+    // If the user's guess is in the current word, log that they chose correctly
+    const didGuessCorrectly = this.currentWord.isLetterFound(letter.choice);
+    if (didGuessCorrectly) {
+      console.info(chalk.green("\nCORRECT!!!\n"));
 
-        console.log(this.currentWord.toString() + "\n");
-      });
+      // Otherwise decrement `guessesLeft`, and let the user know how many guesses they have left
+    } else {
+      this.guessesLeft--;
+      console.info(chalk.red("\nINCORRECT!!!\n"));
+      console.info(this.guessesLeft + " guesses remaining!!!\n");
+    }
+
+    console.info(this.currentWord.toString() + "\n");
   }
 
   // Logs goodbye and exits the node app
   quit() {
-    console.log("\nGoodbye!");
+    console.info("\nGoodbye!");
     process.exit(0);
   }
 }
